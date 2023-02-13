fec67a0762d5011bde01281af208221cdd65c1ca
Kirtan113 <122096760+Kirtan113@users.noreply.github.com>
2023-02-13 22:04:42 +0530

    Add files via upload

---
+++ Practice/conditional statement/else-if.c
@@ -0,0 +1,22 @@
+//Use of  else..if conditional statment
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter value of a and b:");
+    scanf("%d%d",&a,&b);
+    if (a>b)
+    {
+        printf("a is maximum.");
+    }
+    else if (a==b)
+    {
+        printf("a and b both are equel.");
+    }
+    
+    else
+    {
+        printf("b is maximum.");
+    }
+    
+}
